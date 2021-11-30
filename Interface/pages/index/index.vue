6c6e73cda39d247c9cf6a1a84ecba08671f81f25
llr104 <947491240@qq.com>
2021-12-01 00:34:21 +0800

    nav ctrl

--- Interface/pages/index/index.vue
+++ Interface/pages/index/index.vue
@@ -1,6 +1,19 @@
 <template>
 	<view class="container">
-		<product-list></product-list>
+		<!-- <product-list></product-list> -->
+		<image class="nav-ctrl" src="../../static/nav-ctrl.svg" mode="" @click="onClickNavCtrl"></image>
+	
+		<uni-drawer ref="showRight" mode="left" :mask-click="false">
+			<scroll-view style="height: 100%;" scroll-y="true">
+				<button @click="closeDrawer" type="primary">关闭Drawer</button>
+				<uni-list>
+					<uni-list-item title="Home" note="" clickable @click="gotoPage('/pages/index/index')"></uni-list-item>
+					<uni-list-item title="Market" note="" clickable @click="gotoPage('/pages/market/market')"></uni-list-item>
+					<uni-list-item title="My" note="" clickable @click="gotoPage('/pages/my/my')"></uni-list-item>
+				</uni-list>
+			</scroll-view>
+		</uni-drawer>
+
 	</view>
 </template>
 
@@ -17,7 +30,27 @@
 			}
 		},
 		methods: {
-
+			onClickNavCtrl:function(){
+				console.log("onClickNavCtrl");
+				this.showDrawer();
+			},
+			
+			showDrawer() {
+			    this.$refs.showRight.open();
+			},
+			closeDrawer() {
+			    this.$refs.showRight.close();
+			},
+			
+			gotoPage:function(page){
+				console.log("gotoPage:", page)
+				uni.navigateTo({
+					url:page,
+					complete:function(r){
+						console.log(r);
+					}
+				})
+			}
 		}
 	}
 </script>
@@ -27,4 +60,27 @@
 		font-size: 14px;
 		line-height: 24px;
 	}
+	
+	.nav-ctrl {
+		width: 60rpx;
+		height: 60rpx;
+	}
+	
+	.uni-list-item {
+		width: 200rpx;
+		height: 100rpx;
+	}
+	
+	.uni-list-item {
+		width: 200rpx;
+		height: 100rpx;
+	}
+	
+	.nav-item {
+		display: block;
+		width: 100%;
+		height: 100%;
+		background-color: #000000;
+	}
+	
 </style>
