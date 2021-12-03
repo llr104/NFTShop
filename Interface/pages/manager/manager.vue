0deaaf764c4b64a288a779c436c68fb50154027f
llr104 <lianglirong1991@gmail.com>
2021-12-03 18:12:26 +0800

    Update manager.vue

--- Interface/pages/manager/manager.vue
+++ Interface/pages/manager/manager.vue
@@ -6,7 +6,18 @@
 		</v-tabs>
 		
 		<view class="ptdz" v-if="current==0">
-			<text>普通锻造</text>
+			<uni-forms :modelValue="ptdzData">
+				<uni-forms-item required name="name" label="NFT名字">
+					<uni-easyinput type="text" v-model="ptdzData.name" placeholder="请输入NFT名字" />
+				</uni-forms-item>
+				<uni-forms-item required name="des" label="NFT描述">
+					<uni-easyinput type="text" v-model="ptdzData.des" placeholder="请输入NFT描述" />
+				</uni-forms-item>
+				<uni-forms-item required name="count" label="NFT数量">
+					<uni-easyinput type="number" v-model="ptdzData.count" placeholder="请输入NFT数量" />
+				</uni-forms-item>
+			</uni-forms>
+			<button @click="cllickPTDZ">普通锻造</button>
 		</view>
 		<view class="mhdz" v-if="current==1">
 			<text>盲盒锻造</text>
@@ -34,18 +45,31 @@
 		data() {
 			return {
 				current: 0,
-				tabs: ['普通锻造', "盲盒锻造",'销毁', '查询']
+				tabs: ['普通锻造', "盲盒锻造",'销毁', '查询'],
+				ptdzData:{
+					name: "",
+					des: "",
+					count: 0
+				}
 			};
 		},
 		
 		methods:{
 			changeTab(index) {
 				console.log('当前选中的项：' + index)
+			},
+			
+			cllickPTDZ:function(){
+				console.log("cllickPTDZ:", this.ptdzData);
 			}
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+	.ptdz {
+		margin-top: 2rpx;
+		padding: 50rpx;
+		background-color: #FFFFFF;
+	}
 </style>
