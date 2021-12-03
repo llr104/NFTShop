de61b768ac7eb5f109eaba62cdd6c0d2546a6d69
llr104 <lianglirong1991@gmail.com>
2021-12-03 16:35:30 +0800

    我的

--- Interface/pages/my/my.vue
+++ Interface/pages/my/my.vue
@@ -1,24 +1,44 @@
 <template>
 	<view>
 		<navigation></navigation>
+		<v-tabs v-model="current" :tabs="tabs" :bold="true" 
+		:lineScale="0.8" fontSize="34rpx" @change="changeTab">
+		</v-tabs>
+		<product-list v-if="current==0" class="plist"></product-list>
+		<product-list v-if="current==1" class="plist"></product-list>
+		
 	</view>
 </template>
 
 <script>
 	import navigation from "../../components/navigation/navigation.vue";
+	import vtab from "../../components/v-tabs/v-tabs.vue";
+	import productList from "../../components/product-list/product-list.vue";
+	
 	export default {
 		components:{
-			navigation
+			navigation,
+			vtab,
+			productList
 		},
 		
 		data() {
 			return {
-				
+				current: 0,
+				tabs: ['我拥有的', '我卖出的']
 			};
+		},
+		
+		methods:{
+			changeTab(index) {
+				console.log('当前选中的项：' + index)
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
+	.plist {
+		margin-top: 20rpx;
+	}
 </style>
